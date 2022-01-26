99c397fe12d578afeecc1dae48ce5a97c872e925
Yaakov Yisrael Benjamin <yyisrael613@gmail.com>
2022-01-25 20:55:01 -0500

    created questions array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 // initialize variables
 var time = document.querySelector('#time');
 var intro = document.querySelector('#intro');
@@ -10,37 +9,58 @@ var secs = 0;
 // display seconds in timer
 time.textContent = secs;
 
-// game over message
+// Questions Array
+var arrQuestions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        options: ["strings", "booleans", "alerts", "numbers"],
+        correctAns: "alerts"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within ____.",
+        options: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        correctAns: "parenthesis"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____.",
+        options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        correctAns: "all of the above"
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        options: ["commas", "curly brackets", "quotes", "parenthesis"],
+        correctAns: "quotes"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: ["JavaScript", "terminal / bash", "for loops", "console log"],
+        correctAns: "console log"
+    }
+];
+
+// Game Over message
 function displayFinished() {
     finalMessage.style.display = "block";
 }
 
 // if "Start Quiz" is pressed
 startbtn.addEventListener("click", function() {
-    intro.style.display = "none";
+    intro.style.display = "none"; // hide intro
 
-    secs = 1;
+    secs = 1; // set starting time
 
-    var timerInterval = setInterval(function () {
-        time.textContent = secs;
-        secs--;
+    var timerInterval = setInterval(function () { // COUNTDOWN
+        time.textContent = secs; // display seconds
+        secs--;                  // decrement by 1
 
-        if (secs < 0) { // display finished message
-            clearInterval(timerInterval);
-            displayFinished();
+        if (secs < 0) {
+            clearInterval(timerInterval); // stop timer
+            displayFinished(); // display finished message
         }
     }, 1000);
 });
 
 
-
-var arrQuestions = [
-    {
-        question: "what is 2+2?",
-        options: ["1", "2", "3", "4"],
-        correctAns: "4"
-    }
-];
 
 
 /*
@@ -48,16 +68,3 @@ var arrQuestions = [
     var numCorrect = 0;
     var numIncorrect = 0;
 */
-
-
-
-    // var h1El = document.createElement("h1");
-    // h1El.textContent = "All done!";
-    // document.body.appendChild(h1El);
-
-    // var h2El = document.createElement("h2");
-    // h2El.textContent = "Your final score is " + score + '.';
-    // document.body.appendChild(h2El);
-
-    // var initials = document.createElement("h2");
-   // initials.textContent = "Enter initials: " + [INPUT BOX] + [SUBMIT];
